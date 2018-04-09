15c29ef9b73b4d3679ee9b1d3899a250c4003309
Mahesh Jaipal <mahesh.max.kumar86@gmail.com>
2018-04-09 21:28:34 +0530

    --added

--- designs.js
+++ designs.js
@@ -4,7 +4,25 @@
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
+  var row,column,tableElem,rowElem,colElem;
+  row =$('#inputHeight').val();
+  column = $('#inputWeight').val();
 
-// Your code goes here!
 
-}
+    for (var i = 0; i < row; i++) {
+            // rowElem = document.createElement('tr');
+            $('#pixelCanvas').append('<tr></tr>')
+            for (var j = 0; j < column; j++) {
+                // colElem = document.createElement('td');
+                // rowElem.append(colElem);
+              $('tr').append( '<td>' + 'result' +  i + '</td>' );
+            }
+
+            // tableElem.append(rowElem);
+        }
+
+        // document.body.append(tableElem);
+};
+$('#submit').click(function x() {
+    makeGrid();
+});
