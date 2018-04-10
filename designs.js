9eaac3008fef85c712e4e1ac88b69b293c140e01
Mahesh Jaipal <mahesh.max.kumar86@gmail.com>
2018-04-11 00:39:44 +0530

    Color filled in pixels

--- designs.js
+++ designs.js
@@ -2,23 +2,34 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-
+//
 function makeGrid() {
     var row,column,tableElem,rowElem,colElem;
     row =$('#inputHeight').val();
     column = $('#inputWeight').val();
     tableElem = document.createElement('table');
+    var att = document.createAttribute("id");
+    att.value = "Grid";
+    tableElem.setAttributeNode(att);
     for (var i = 0; i < row; i++) {
             rowElem = document.createElement('tr');
             for (var j = 0; j < column; j++) {
-                colElem = document.createElement('td');
+                colElem = document.createElement('td')
+                var event = document.createAttribute("onClick");
+                event.value = "clickFn()";
+                colElem.setAttributeNode(event);
                 rowElem.append(colElem);
               }
             tableElem.append(rowElem);
         }
         $('#pixelCanvas').html(tableElem);
 };
-$('#submit').click(function x() {
-
-      makeGrid();
+$('#submit').click(function () {
+  makeGrid();
 });
+function clickFn() {
+  $("#Grid td").click(function() {
+            var colorValue = $('#colorPicker').val();
+            $(this).css('background-color',colorValue);
+  });
+}
